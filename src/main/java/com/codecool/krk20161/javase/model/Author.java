395352a5fd640a1666f37fcf667620bc7f033aba
totine <joanna.gargas@gmail.com>
2017-06-05 11:17:05 +0200

    Add biography field and new constructor (name,biography) in Author model

--- src/main/java/com/codecool/krk20161/javase/model/Author.java
+++ src/main/java/com/codecool/krk20161/javase/model/Author.java
@@ -5,8 +5,14 @@ package com.codecool.krk20161.javase.model;
  */
 public class Author {
     private String name;
+    String biography;
 
     public Author(String name) {
         this.name = name;
+    }
+
+    public Author(String name, String biography) {
+        this.name = name;
+        this.biography = biography;
     }
 }
