934a22b72dde113548763a254729fa9358f24b5b
totine <joanna.gargas@gmail.com>
2017-06-05 10:26:28 +0200

    Add summary field and overloaded constructor in Book model

--- src/main/java/com/codecool/krk20161/javase/model/Book.java
+++ src/main/java/com/codecool/krk20161/javase/model/Book.java
@@ -7,9 +7,16 @@ public class Book {
 
     private String title;
     private Author author;
+    private String summary;
 
     public Book(String title, Author author) {
         this.title = title;
         this.author = author;
+    }
+
+    public Book(String title, Author author, String summary) {
+        this.title = title;
+        this.author = author;
+        this.summary = summary;
     }
 }
