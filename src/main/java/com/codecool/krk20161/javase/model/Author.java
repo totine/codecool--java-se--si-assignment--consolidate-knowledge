0379487aa925ac069d985606092eb4570e0c9ad5
totine <joanna.gargas@gmail.com>
2017-06-05 11:14:43 +0200

    Add name field and constructor with name parameter in Author model

--- src/main/java/com/codecool/krk20161/javase/model/Author.java
+++ src/main/java/com/codecool/krk20161/javase/model/Author.java
@@ -4,4 +4,9 @@ package com.codecool.krk20161.javase.model;
  * Created by joanna on 05.06.17.
  */
 public class Author {
+    private String name;
+
+    public Author(String name) {
+        this.name = name;
+    }
 }
