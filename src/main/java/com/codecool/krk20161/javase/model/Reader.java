8d133a8c2cf38ef2ed35fd0b5e2a7220aed253a8
totine <joanna.gargas@gmail.com>
2017-06-05 22:49:53 +0200

    Add BookSearch interface and implements it to Catalog and Reader models

--- src/main/java/com/codecool/krk20161/javase/model/Reader.java
+++ src/main/java/com/codecool/krk20161/javase/model/Reader.java
@@ -1,7 +1,22 @@
 package com.codecool.krk20161.javase.model;
 
+import com.codecool.krk20161.javase.util.BookSearch;
+
+import java.util.List;
+
 /**
  * Created by joanna on 05.06.17.
  */
-public class Reader {
+public class Reader implements BookSearch {
+
+
+    @Override
+    public List<Book> searchByTitle(String title) {
+        return null;
+    }
+
+    @Override
+    public List<Book> searchByAuthor(Author author) {
+        return null;
+    }
 }
