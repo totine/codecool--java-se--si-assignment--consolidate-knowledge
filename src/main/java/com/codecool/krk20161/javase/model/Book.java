7434427224af7aaff9ca2f3d565e2fb626bac694
totine <joanna.gargas@gmail.com>
2017-06-05 10:25:23 +0200

    Add fields (title, autor) and first constructor in Book model

--- src/main/java/com/codecool/krk20161/javase/model/Book.java
+++ src/main/java/com/codecool/krk20161/javase/model/Book.java
@@ -4,4 +4,12 @@ package com.codecool.krk20161.javase.model;
  * Created by joanna on 05.06.17.
  */
 public class Book {
+
+    private String title;
+    private Author author;
+
+    public Book(String title, Author author) {
+        this.title = title;
+        this.author = author;
+    }
 }
