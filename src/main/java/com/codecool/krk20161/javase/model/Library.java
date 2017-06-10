6acab551a9f867860b3ad98c5852e55b091b200b
totine <joanna.gargas@gmail.com>
2017-06-11 00:05:48 +0200

    Add constructor in Library

--- src/main/java/com/codecool/krk20161/javase/model/Library.java
+++ src/main/java/com/codecool/krk20161/javase/model/Library.java
@@ -4,6 +4,10 @@ package com.codecool.krk20161.javase.model;
 public class Library {
     
     private Catalog catalog;
+
+    public Library() {
+        catalog = new Catalog();
+    }
     
     public Catalog getCatalog() {
         return catalog;
