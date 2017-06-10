168dd7e0b897a7458984bd450924f326b60222c1
totine <joanna.gargas@gmail.com>
2017-06-10 17:56:40 +0200

    Add catalog field, getter and setter in Library class

--- src/main/java/com/codecool/krk20161/javase/model/Library.java
+++ src/main/java/com/codecool/krk20161/javase/model/Library.java
@@ -2,4 +2,14 @@ package com.codecool.krk20161.javase.model;
 
 
 public class Library {
+    
+    private Catalog catalog;
+    
+    public Catalog getCatalog() {
+        return catalog;
+    }
+
+    public void setCatalog(Catalog catalog) {
+        this.catalog = catalog;
+    }
 }
