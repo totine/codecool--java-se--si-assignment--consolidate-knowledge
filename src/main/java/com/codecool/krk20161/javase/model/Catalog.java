82f11c9251bbfbc077e3c7ef5e365ebfa8cde2e2
totine <joanna.gargas@gmail.com>
2017-06-10 17:55:09 +0200

    Add mehtods structure in Catalog class

--- src/main/java/com/codecool/krk20161/javase/model/Catalog.java
+++ src/main/java/com/codecool/krk20161/javase/model/Catalog.java
@@ -3,10 +3,13 @@ package com.codecool.krk20161.javase.model;
 
 import com.codecool.krk20161.javase.util.BookSearch;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Catalog implements BookSearch{
+    
+    private ArrayList<Book> books;
 
     @Override
     public List<Book> searchByTitle(String title) {
@@ -16,5 +19,15 @@ public class Catalog implements BookSearch{
     @Override
     public List<Book> searchByAuthor(Author author) {
         return null;
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public void add(Book book) {
+    }
+
+    public void remove(Book bookToRemove) {
     }
 }
