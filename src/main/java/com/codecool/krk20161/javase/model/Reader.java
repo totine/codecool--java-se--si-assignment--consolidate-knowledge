6f32f7f9386a39752c493a441d3d610f81394768
totine <joanna.gargas@gmail.com>
2017-06-10 17:59:31 +0200

    Add constructors, name and read fields and getters in Reader class

--- src/main/java/com/codecool/krk20161/javase/model/Reader.java
+++ src/main/java/com/codecool/krk20161/javase/model/Reader.java
@@ -2,11 +2,21 @@ package com.codecool.krk20161.javase.model;
 
 import com.codecool.krk20161.javase.util.BookSearch;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Reader implements BookSearch {
+    
+    private String name;
+    private ArrayList<Book> read;
 
+
+    public Reader(String name) {
+    }
+
+    public Reader(String name, List<Book> read) {
+    }
 
     @Override
     public List<Book> searchByTitle(String title) {
@@ -16,5 +26,13 @@ public class Reader implements BookSearch {
     @Override
     public List<Book> searchByAuthor(Author author) {
         return null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Book> getRead() {
+        return read;
     }
 }
